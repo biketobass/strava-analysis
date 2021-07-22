3f820584bd8ccc57329220aaa4154a60b3fd1310
Bike to Bass <78823174+biketobass@users.noreply.github.com>
2021-07-22 18:01:15 -0400

    A few README clarifications and added examples to get_strava_activities

--- get_strava_activities.py
+++ get_strava_activities.py
@@ -7,12 +7,19 @@ sa = strava.StravaAnalyzer()
 #sa.get_strava_profile()
 
 # Get the predicted average speed for a route with a particular
-# elevation gain and distance. Also include a lower bound on speed,
+# elevation gain and distance. This also includes a lower bound on speed,
 # a lower bound on distance, and a year from which to start basing your model.
 # It will use data from the start year to the present. If you want all of your data included,
 # omit everything except elevation gain and distance. They are the only two required parameters.
 
-#sa.predict_avg_speed(2000, 30, lower_speed_filter=14, lower_distance_filter=20, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1)
+#sa.predict_avg_speed(2000, 30, lower_speed_filter=15, lower_distance_filter=20, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1)
 
 # This is the same as the above just with the metric flag set to true.
 #sa.predict_avg_speed(609.6, 48.2803, lower_speed_filter=22.5308, lower_distance_filter=32.1869, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1, metric=True)
+
+# Examples of using suggest_similar_activities
+#sa.suggest_similar_activities(elev_gain=200, distance=50, elev_fudge=0.1, dist_fudge=0.1, activity_type="Ride")
+
+#sa.suggest_similar_activities(distance=5, activity_type="Walk")
+
+#sa.predict_avg_speed(elev_gain=364, distance=10, activity_type="Hike")
