477867b9cd58964a7cda79a2aae31071d0ef568b
Bike to Bass <78823174+biketobass@users.noreply.github.com>
2021-07-23 10:58:02 -0400

    Fixed typo in first two predict_avg_speed calls

--- get_strava_activities.py
+++ get_strava_activities.py
@@ -3,6 +3,8 @@ import strava
 # First create a StravaAnalyzer object
 sa = strava.StravaAnalyzer()
 
+
+# ----------- for GitHub
 # Uncomment the line below if you want to retrieve your Strava profile data.
 #sa.get_strava_profile()
 
@@ -12,10 +14,10 @@ sa = strava.StravaAnalyzer()
 # It will use data from the start year to the present. If you want all of your data included,
 # omit everything except elevation gain and distance. They are the only two required parameters.
 
-#sa.predict_avg_speed(2000, 30, lower_speed_filter=15, lower_distance_filter=20, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1)
+#sa.predict_avg_speed(elev_gain=2000, distance=30, lower_speed_filter=15, lower_distance_filter=20, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1)
 
 # This is the same as the above just with the metric flag set to true.
-#sa.predict_avg_speed(609.6, 48.2803, lower_speed_filter=22.5308, lower_distance_filter=32.1869, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1, metric=True)
+#sa.predict_avg_speed(elev_gain=609.6, distance=48.2803, lower_speed_filter=22.5308, lower_distance_filter=32.1869, model_start_year=2020, dist_fudge=0.1, elev_fudge=0.1, metric=True)
 
 # Examples of using suggest_similar_activities
 #sa.suggest_similar_activities(elev_gain=200, distance=50, elev_fudge=0.1, dist_fudge=0.1, activity_type="Ride")
